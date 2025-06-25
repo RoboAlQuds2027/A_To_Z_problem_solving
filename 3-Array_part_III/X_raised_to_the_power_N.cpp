521b1ee04e2cca295642b21e6eee9bc22b558e04
mahmoud <mahmoudsaieed300@gmail.com>
2025-06-25 13:45:08 +0800

    3-Array_part_III / X_raised_to_the_power_N.cpp

---
+++ 3-Array_part_III/X_raised_to_the_power_N.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+double myPow(double x, int n) {
+      double ans = 1.0;
+      for (int i = 0; i < n; i++) {
+        ans = ans * x;
+      }
+      return ans;
+}
+int main()
+{
+    cout<<myPow(2.1,3)<<endl;
+}
